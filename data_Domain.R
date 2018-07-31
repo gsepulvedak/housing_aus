0456426b72c952f9b1f68a4f3b8f01423c8e448b
Gonzalo Raul Sepulveda Kattan <gonzalo.sepulveda@sma.gob.cl>
2018-07-31 18:08:46 -0400

    Se corrige url y se agregan más extracciones

--- data_Domain.R
+++ data_Domain.R
@@ -6,8 +6,10 @@ library(lubridate)
 
 
 #url <- "https://www.domain.com.au/rent/?suburb=caulfield-vic-3162,south-yarra-vic-3141,windsor-vic-3181,st-kilda-east-vic-3183,caulfield-north-vic-3161,prahran-vic-3181,prahran-east-vic-3181,toorak-vic-3142,elsternwick-vic-3185,balaclava-vic-3183,armadale-vic-3143,elwood-vic-3184,st-kilda-vic-3182&bedrooms=2&price=0-400&ssubs=1"
-url <- "https://www.domain.com.au/rent/?suburb=caulfield-vic-3162&bedrooms=2&price=0-400&ssubs=1"
+url <- "https://www.domain.com.au/rent/?suburb=caulfield-vic-3162&bedrooms=2&price=0-400&ssubs=1&page=1"
 doc <- read_html(url)
 doc_parsed <- htmlParse(doc)
-price <- xpathSApply(doc_parsed, "//div/p", xmlValue)
-link <- xpathSApply(doc_parsed, "//div/a[@href]", xmlValue)
+price <- xpathSApply(doc_parsed, "//div/p[@class = 'listing-result__price']", xmlValue)
+link <- xpathSApply(doc_parsed, "//div/link[@itemprop='url']", xmlGetAttr, "href")
+address <- xpathSApply(doc_parsed, "//div/a/h2/span[@class = 'address-line1']", xmlValue)
+suburb <- xpathSApply(doc_parsed, "//div/a/h2/span/span[@itemprop='addressLocality']", xmlValue)
