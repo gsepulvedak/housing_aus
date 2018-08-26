4c1d63743138523d961ffc2c7667c870450a55d1
Gonzalo Sepulveda <gsepulvedak@gmail.com>
2018-08-25 23:19:59 -0300

    Se completa código. Falta envío de correo.

--- update_Domain.R
+++ update_Domain.R
@@ -5,6 +5,7 @@ library(stringr)
 library(lubridate)
 library(googledrive)
 library(googlesheets)
+library(gmailr)
 
 #URL inicial a buscar
 #url <- "https://www.domain.com.au/rent/?suburb=caulfield-vic-3162,south-yarra-vic-3141,windsor-vic-3181,st-kilda-east-vic-3183,caulfield-north-vic-3161,prahran-vic-3181,prahran-east-vic-3181,toorak-vic-3142,elsternwick-vic-3185,balaclava-vic-3183,armadale-vic-3143,elwood-vic-3184,st-kilda-vic-3182&bedrooms=2&price=0-400&ssubs=1"
@@ -65,11 +66,32 @@ updated_data <- bind_rows(updated_data)
 old_data <- gs_title("DomainHousing") %>% gs_read(col_types = "cccccccDc") 
 
 #Identificación de registros no vigentes y actualización
-Incierto <- dplyr::setdiff(old_data$Link, updated_data$Link)
-old_data <- old_data %>% select(-Status) %>% mutate(Status = ifelse(.$Link %in% Incierto, "Incierto", "Valid"))
-rm(Incierto)
+NoValid <- dplyr::setdiff(old_data$Link, updated_data$Link)
+old_data <- old_data %>% select(-Status) %>% mutate(Status = ifelse(.$Link %in% NoValid, "Not valid", "Valid"))
+rm(NoValid)
 
 #Identificación de registros nuevos y adición en dataset
 old_data_tmp <- old_data %>% select(-Date, -Status)
 new_rows <- dplyr::setdiff(updated_data, old_data_tmp) %>% mutate(Date = today(), Status = "Valid")
 new_data <- bind_rows(old_data, new_rows)
+
+#Limpieza
+rm(old_data, old_data_tmp, updated_data)
+
+#Actualización googlesheets de drive
+id_old <- drive_find(pattern = "DomainHousing", type = "spreadsheet")[2] %>% as.character()
+drive_rm(as_id(id_old))
+tmp <- tempfile(fileext = ".csv")
+write_csv(new_data, path = tmp)
+drive_upload(tmp, path = as_id("1baCzvRlccVIjazbLLl8HF5i4T7lOZuEN"), type = "spreadsheet", name = "DomainHousing")
+
+#Envío de mail con novedades del último proceso
+gmail_auth("compose")
+
+msg <- mime() %>% 
+  from("Proceso Domain") %>% 
+  to(c("gsepulvedak@gmail.com")) %>% 
+  subject("Nuevas opciones en Melbourne!") %>% 
+#  text_body(paste0("Se ha realizado una nueva actualización en la carpeta opendata de RILES.\nMensaje de la operación:\n\n", drive_msg, "\n\nRevisar en https://drive.google.com/drive/u/1/folders/",RILES_id, "\n\nSaludos.", collapse = "\n\n"))
+
+send_message(msg, user_id = "me")
